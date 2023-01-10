cf89c8d3e6db0a53cc89c3e50260d8d506607aef
Tommy Kang <tommykang77@naver.com>
2023-01-10 09:07:04 +0000

    Complete GraphColouring2021 Parts 1-2

--- GraphColouring2021/GraphColouring.hs
+++ GraphColouring2021/GraphColouring.hs
@@ -11,28 +11,40 @@ import Examples
 -- Part I
 --
 count :: Eq a => a -> [a] -> Int
-count 
-  = undefined
+count
+  = (length .) . elemIndices
 
 degrees :: Eq a => Graph a -> [(a, Int)]
-degrees 
-  = undefined
+degrees (ns, es)
+  = map (\n -> (n, count n flat)) ns
+  where
+    flat = concatMap (\(x, y) -> [x, y]) es
 
 neighbours :: Eq a => a -> Graph a -> [a]
-neighbours 
-  = undefined
+neighbours n (ns, es)
+  = [if (n == n') then n'' else n' | (n', n'') <- es, n == n' || n == n'']
 
 removeNode :: Eq a => a -> Graph a -> Graph a
-removeNode 
-  = undefined
+removeNode n (ns, es)
+  = (delete n ns, [e | e@(n', n'') <- es, n /= n' && n /= n''])
 
 ------------------------------------------------------
 --
 -- Part II
 --
 colourGraph :: (Ord a, Show a) => Int -> Graph a -> Colouring a
-colourGraph 
-  = undefined
+colourGraph c ([], [])
+  = []
+colourGraph c g@(ns, es)
+  = (n, givenc) : colours
+  where
+    degs    = degrees g
+    (n, d)  = foldl1 (\k@(_, d) k'@(_, d') -> if (d' < d) then k' else k) degs
+    nbs     = neighbours n g
+    colours = colourGraph c (removeNode n g)
+    takencs = [c | (n, c) <- colours, n `elem` nbs]
+    givenc  = head (([1..c] \\ takencs) ++ [0])
+
 
 ------------------------------------------------------
 --
